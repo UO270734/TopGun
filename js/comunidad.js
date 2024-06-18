90a7b244ad4c1a32e9a834197d42ab1b160505b5
Mauro Varea Fernández <uo270734@uniovi.es>
2024-06-18 22:43:37 +0200

    Meteorología terminado

--- js/comunidad.js
+++ js/comunidad.js
@@ -6,10 +6,9 @@ class Comunidad {
         this.lonCapital = lonCapital;
 		this.poblacion = poblacion;
 
-		this.apiKey = '8fa3dabcab848bd262b8fee40ae456b5';
-		this.apiUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily';
-		this.idValencia = 2509954;
-		this.days = 7;
+		this.apiKey = '8fa3dabcab848bd262b8fee40ae456b5';		
+		this.apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+		//this.apiUrl = 'https://api.openweathermap.org/data/2.5/onecall';
     }
 
     getName() {
@@ -24,28 +23,69 @@ class Comunidad {
         return this.poblacion.toString();
     }
 
-	getMeteo() {
-		const url = `${this.apiUrl}?id=${this.cityId}&cnt=${this.days}&units=metric&appid=${this.apiKey}`;
-        $.getJSON(url, (data) => {
-            this.displayWeatherData(data);
-        }).fail(() => {
-            $('#prevision-meteorologica').html('<p>Error al obtener los datos meteorológicos.</p>');
-        });
-    }
+	mostrarMeteorologia() {
+        var url = `${this.apiUrl}?lat=${this.latCapital}&lon=${this.lonCapital}&units=metric&appid=${this.apiKey}`;
 
-    displayWeatherData(data) {
-        let htmlContent = '';
-        data.list.forEach(day => {
-            const date = new Date(day.dt * 1000).toLocaleDateString();
-            htmlContent += `
-                <article>
-                    <h3>${date}</h3>
-                    <p>Temperatura: ${day.temp.day} °C</p>
-                    <p>Condición: ${day.weather[0].description}</p>
-                </article>
-            `;
+        $('section').append("<h3>Previsión meteorológica en Valencia para los próximos 5 días</h3>");
+        $.ajax({
+            dataType: 'json',
+            url: url,
+            method: 'GET',
+            success: function (data) {
+                var previsiones = data.list;
+                var dias = [];
+                dias.push(previsiones[0]);
+
+                previsiones.forEach(prevision => {
+                    if (prevision.dt_txt.split(' ')[1] == '12:00:00') {
+                        dias.push(prevision);
+                    }                    
+                });
+                dias.forEach(dia => {
+                    let diaHtml = '<table>';
+                    diaHtml += `<caption>${dia.dt_txt.split(' ')[0]}</caption>`;
+					diaHtml += `<tbody>`
+					diaHtml += `<tr>`
+					diaHtml += `<td>Temperatura máxima</td>`
+					diaHtml += `<td>${dia.main.temp_max} ºC</td>`
+					diaHtml += `<td rowspan="0"><img src="https://openweathermap.org/img/wn/${dia.weather[0].icon}@2x.png" alt="${dia.weather[0].main}"/></td>`
+					diaHtml += `</tr>`
+					diaHtml += `<tr>`
+					diaHtml += `<td>Temperatura mínima</td>`
+					diaHtml += `<td>${dia.main.temp_min}</td>`
+					diaHtml += `</tr>`
+					diaHtml += `<tr>`
+					diaHtml += `<td>Sensación térmica:</td>`
+					diaHtml += `<td>${dia.main.feels_like} ºC</td>`
+					diaHtml += `</tr>`
+					diaHtml += `<tr>`
+					diaHtml += `<td>Humedad</td>`
+					diaHtml += `<td>${dia.main.humidity} %</td>`
+					diaHtml += `</tr>`
+					diaHtml += `<tr>`
+					diaHtml += `<td>Presión atmosférica</td>`
+					diaHtml += `<td>${dia.main.pressure} hPa</td>`
+					diaHtml += `</tr>`
+					diaHtml += `<tr>`
+					diaHtml += `<td>Velocidad del viento</td>`
+					diaHtml += `<td>${dia.wind.speed} m/s</td>`
+					diaHtml += `</tr>`
+                    if (dia.pop !== undefined) {
+						diaHtml += `<tr>`
+						diaHtml += `<td>Probabilidad de precipitación</td>`
+						diaHtml += `<td>${(dia.pop * 100).toFixed(0)}%</td>`
+						diaHtml += `</tr>`
+                    }
+                    diaHtml += `</tbody>`
+					diaHtml += '</table>';     
+
+                    $("section").append(diaHtml);
+                });
+            },
+            error: function() {
+                $('section').append("<p>Ha ocurrido un error al cargar la meteorología.</p>");
+            }
         });
-        $('#prevision-meteorologica').html(htmlContent);
     }
 	
 }
